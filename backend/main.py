5ff66846c8735e0ed00375b8b50ae0f0717b9995
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-17 13:20:41 +0530

    started coding

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
 app = FastAPI(
     title="story game",
     description = "api to generate story",
@@ -11,4 +13,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_cridentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
 )
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
